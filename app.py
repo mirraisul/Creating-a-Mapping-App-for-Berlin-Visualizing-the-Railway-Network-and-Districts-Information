7319a2ad7f64cd29bada34ffdec98664c02f9759
Mir <raisulsust.bd@gmail.com>
2026-09-17 18:17:53 +0200

    update of basemaps

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ st.sidebar.pyplot(fig)
 #first create a select box to create a basemap
 #basemap= st.selectbox('Select a Basemap', ['CartoDB.DarkMatter', 'OpenStreetMap', 'CartoDB.Positron',])
 
-basemap= st.selectbox('Select a Basemap', ['Stadia.StamenTonerDark', 'Stadia.StamenTonerLite', 'OpenStreetMap', ])
+basemap= st.selectbox('Select a Basemap', ['Esri.WorldGrayCanvas', 'OpenStreetMap.DE', 'OpenStreetMap', ])
 m = leafmap.Map(
     layers_control=True,
     draw_control=False,
